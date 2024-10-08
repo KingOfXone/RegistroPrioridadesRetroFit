44e79edb8be2928d02d38e4ba63b2043bd5d071e
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-10-08 04:27:39 -0400

    Agregando El app Module

---
+++ app/src/main/java/edu/ucne/tarea4/data/di/AppModule.kt
@@ -0,0 +1,57 @@
+package edu.ucne.tarea4.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.tarea4.data.local.database.PrioridadDb
+import edu.ucne.tarea4.remote.PrioridadesApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun providePrioridadDb(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            PrioridadDb::class.java,
+            "Prioridad.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    const val BASE_URL = "https://tarea4-ayabhdemhzdpe4hj.canadacentral-01.azurewebsites.net/"
+
+    @Singleton
+    @Provides
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .add(DateAdater())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provicesPrioridadesApi(moshi: Moshi): PrioridadesApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(PrioridadesApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providePrioridadDao(prioridadDb: PrioridadDb) = prioridadDb.prioridadDao()
+    @Provides
+    @Singleton
+    fun provideTicketDao(prioridadDb: PrioridadDb) = prioridadDb.ticketDao()
+}
